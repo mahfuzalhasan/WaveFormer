2bedcfa6c51a707d3c55b2a512efc95974cbb41d
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2026-01-28 23:57:57 -0500

    print statements removed

--- network_models/network_backbone.py
+++ network_models/network_backbone.py
@@ -401,7 +401,7 @@ class Waveformer(nn.Module):
 
         # Channel Calibration
         dec5 = self.encoder10(outs[3])
-        print(f'after bottleneck layer: {dec5.shape}')
+        
         # Decoder
         dec4 = self.decoder4(dec5, enc3, outs_hf[-1])
         dec3 = self.decoder3(dec5, enc2, outs_hf[-2])
--- network_models/waveformer.py
+++ network_models/waveformer.py
@@ -285,7 +285,7 @@ class MultiscaleTransformer(nn.Module):
         # Stage 1
         
         x1 = rearrange(x0, "b c d h w -> b d h w c")
-        print(f'\n\n ####### input to stage 1: {x1.shape}')
+        # print(f'\n\n ####### input to stage 1: {x1.shape}')
         for blk in self.block1:
             x1, x_h = blk(x1)
         x1_out = rearrange(x1, "b d h w c -> b c d h w")
@@ -296,7 +296,7 @@ class MultiscaleTransformer(nn.Module):
         # Stage 2
         
         x2 = self.downsample_1(x1)
-        print(f'\n\ninput to stage 2: {x2.shape}')
+        # print(f'\n\ninput to stage 2: {x2.shape}')
         for blk in self.block2:
             x2, x_h = blk(x2)
         x2_out = rearrange(x2, "b d h w c -> b c d h w")
@@ -307,7 +307,7 @@ class MultiscaleTransformer(nn.Module):
         # Stage 3
         
         x3 = self.downsample_2(x2)
-        print(f'\n\ninput to stage 3: {x3.shape}')
+        # print(f'\n\ninput to stage 3: {x3.shape}')
         for blk in self.block3:
             x3, x_h = blk(x3)
         x3_out = rearrange(x3, "b d h w c -> b c d h w")
@@ -317,7 +317,7 @@ class MultiscaleTransformer(nn.Module):
 
         # Stage 4
         x4 = self.downsample_3(x3)
-        print(f'\n\ninput to stage 4: {x4.shape}')
+        # print(f'\n\ninput to stage 4: {x4.shape}')
         for blk in self.block4:
             x4, x_h = blk(x4)
             # if isinstance(x4, tuple):
