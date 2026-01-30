4834c3a9cbdd9e8af1dbccd326016d47d578965c
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2026-01-30 04:13:55 -0500

    increased run time in bash. file names changed.

--- bash_brats.sh
+++ bash_brats.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=brats-waveformer
+#SBATCH --job-name=brats-waveformer_2x2-train
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=mdmahfuzalhasan@ufl.edu
 #SBATCH --account=brain-lab
 #SBATCH --qos=brain-lab
-#SBATCH --output=/blue/brain-lab/mdmahfuzalhasan/scripts/WaveFormer/results/brats_compressed_4x4.%J.out
-#SBATCH --error=/blue/brain-lab/mdmahfuzalhasan/scripts/WaveFormer/results/brats_compressed_4x4.%J.err
+#SBATCH --output=/blue/brain-lab/mdmahfuzalhasan/scripts/WaveFormer/results/brats_compressed_2x2.%J.out
+#SBATCH --error=/blue/brain-lab/mdmahfuzalhasan/scripts/WaveFormer/results/brats_compressed_2x2.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
